30e893097299a20bb7edbbb38e839805f3aa9030
rodrick <lankfordr51@gmail.com>
2024-03-15 15:16:02 -0500

    commit

---
+++ test.java
@@ -0,0 +1,58 @@
+public test{
+    public static void main(String args[]){
+        //solution 1
+        for(int i =0; i<n.length;i++){
+            if(i%2 = 0){
+                System.out.println(i)
+            }
+        }
+
+        //solution 2
+        Scanner sc = new Scanner();
+        int a[] = sc.next();
+        sum = 0;
+        for(int v : a){
+            sum += v
+        }
+        System.out.println(sum)
+        System.out.println(sum/a.length)
+
+        //solution 3
+        public static void(num1,num2, op){
+            if(op == "+"){
+                res = num1 + num2;
+            }else if(op == '-'){
+                res = num1 - num2;
+            }else if (op == '*'){
+                res= num1*num2
+            }else{
+                res = num1/num2
+            }
+        }
+        //solution 4
+        System.out.println("Input a number between 1-50")
+        Scannner sc = new Scanner();
+        int num = sc.nextint();
+        for(int arrayNum : array){
+            if(num == arrayNum){
+                System.out.println("Bingo"
+            }
+        }
+        //solution 5
+        for(String word : words){
+		word = word.reverse()
+		int left = 0
+		int right = words.length -1
+		while (left < right) {
+            		char temp = words[left];
+            		words[left] = words[right];
+            		words[right] = temp;
+            		left++;
+            		right--;
+       		 }
+        }
+        Arrays.sort(words)
+
+
+    }
+}
